71d9a4e6ae95457dd175fdbf4278ce35b2a589cf
level-128 <Wang740305@outlook.com>
2021-04-15 04:46:42 +0800

    add doc for runlib.keymacro

--- runlib/key_macro.py
+++ runlib/key_macro.py
@@ -1,3 +1,30 @@
+"""
+runlib.key_macro
+this module handles keyboard event.
+meowencrypt supports shortcut key to perform certain actions. This file
+defined keyboard macro which triggered by keyboard shortcut.
+
+When the boot.py executes, method start_keyboard_listen() will be called,
+creating a thread and listening shortcuts.
+
+
+COPYRIGHT NOTICE:
+Copyright (C) 2021  level-128
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import keyboard
 import mouse
 import time
