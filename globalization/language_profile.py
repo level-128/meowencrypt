01ef5c9ee5df5a1f82ba1e17d57603881705a4f1
level-128 <wang740305@outlook.com>
2021-04-14 23:42:45 +0800

    feat: add multilingual framework support

--- globalization/language_profile.py
+++ globalization/language_profile.py
@@ -3,21 +3,6 @@ import json
 import sys
 from config.config_library import config
 
-#  current supported language:
-
-current_locale: str = config.language
-if current_locale == 'default':
-	current_locale = locale.getdefaultlocale()[0]
-
-with open(f'globalization\\languages\\all_locals', 'r') as file:
-	_all_locals = json.loads(file.read())
-
-if current_locale not in _all_locals.values():
-	raise Exception(f"the selected language {current_locale} does not exist")
-
-with open(f'globalization\\languages\\{current_locale}.lan', 'r') as file:
-	language_file = json.loads(file.read())
-
 
 # print function used for print translated items
 # noinspection PyShadowingBuiltins
@@ -37,5 +22,19 @@ def _(english_original_text: str) -> str:
 	return language_file.get(english_original_text, english_original_text)
 
 
+current_locale: str = config.language
+if current_locale == 'default':
+	current_locale = locale.getdefaultlocale()[0]
+
+with open(f'globalization\\languages\\all_locals', 'r') as file:
+	_all_locals = json.loads(file.read())
+
+if current_locale not in _all_locals.values():
+	raise Exception(f"the selected language {current_locale} does not exist")
+
 if current_locale == 'en_US':  # there is no need to translate when the selected language is en-US.
-	def _(x): return x
+	def _(x):
+		return x
+else:
+	with open(f'globalization\\languages\\{current_locale}.lan', 'r') as file:
+		language_file = json.loads(file.read())
