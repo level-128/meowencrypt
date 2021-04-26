0e1a8c42ebb413ecbd4d2d34abb1796eee4c9f33
level-128 <Wang740305@outlook.com>
2021-04-26 09:40:32 +0800

    finished settings_UI.py

--- UI/settings_UI.py
+++ UI/settings_UI.py
@@ -16,9 +16,9 @@ def show():
 	settings_UI.set_input_box('max session number', default = config.max_session)
 
 	content = settings_UI.show()
-
-	config.is_default_listen_clipboard = content.pop(0)
-	config.language = 'default' if (_ := content.pop(0)) == 'default' else from_name_to_notation(_)
-	config.is_dark_mode = (None, False, True)[darkmode_options.index(content.pop(0))]
-	config.GUI_os_process_dpi_awareness = int(content.pop(0))
-	config.max_session = int(content.pop(0))
+	if content is not None:
+		config.is_default_listen_clipboard = content.pop(0)
+		config.language = 'default' if (_ := content.pop(0)) == 'default' else from_name_to_notation(_)
+		config.is_dark_mode = (None, False, True)[darkmode_options.index(content.pop(0))]
+		config.GUI_os_process_dpi_awareness = int(content.pop(0))
+		config.max_session = int(content.pop(0))
