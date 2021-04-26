26eb951471acdb6be2719289996159b94977b5b2
level-128 <Wang740305@outlook.com>
2021-04-27 05:29:21 +0800

    add build.bat for compile meowencrypt

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 nuitka boot.py --windows-icon-from-ico=files\level-128_avatar_256x256.ico --standalone --output-dir=meowencrypt_build --lto
 mkdir "meowencrypt_build\boot.dist\meowencrypt's license.txt"
 mkdir "meowencrypt_build\boot.dist\meowencrypt's introduction.md"
@@ -8,3 +9,4 @@ copy LICENSE "meowencrypt_build\boot.dist\meowencrypt's license" /y
 copy README.md "meowencrypt_build\boot.dist\meowencrypt's introduction.md" /y
 copy files\* "meowencrypt_build\boot.dist\files" /y
 copy globalization\languages\* "meowencrypt_build\boot.dist\globalization\languages" /y
+PAUSE
