13e5d7f518951a6df56d76e288d6ea27df501556
level-128 <Wang740305@outlook.com>
2021-05-10 08:18:10 +0800

    update builder

--- build/build_nuitka.cmd
+++ build/build_nuitka.cmd
@@ -3,7 +3,7 @@ cd..
 ECHO ====================================
 ECHO compiling meowencrypt using nuitka
 ECHO ====================================
-nuitka boot.py --windows-icon-from-ico=files\level-128_avatar_256x256.ico --standalone --output-dir=meowencrypt_build --lto --full-compat
+nuitka boot.py --windows-icon-from-ico=files\level-128_avatar_256x256.ico --standalone --output-dir=meowencrypt_build --lto
 
 ECHO ====================================
 ECHO complete
